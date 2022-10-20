e1c4fa1864ff7af6b5e59acd9d153107fc4b8414
Parag Bhatnagar <paragbhtngr@gmail.com>
2022-10-20 11:04:39 +0800

    completed 05-hooks

---
+++ src/05-hooks/App.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown";
+import Search from "./components/Search";
+import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
+
+const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+
+  return (
+    <div className="ui container">
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
+    </div>
+  );
+};
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end JavaScript Framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favourite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
+
+const options = [
+  {
+    label: "The Color Red",
+    value: "Red",
+  },
+  {
+    label: "The Color Green",
+    value: "Green",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "Blue",
+  },
+];
+
+export default App;
